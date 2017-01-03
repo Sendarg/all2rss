960b1e85475ac5a0f1f9889dc00262286e7f3cdb
Sendarg Lee <Sendarg@users.noreply.github.com>
2017-01-03 22:47:25 +0800

    Current: Fuck  Docker

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# ready pip
+pip install -r requirements.txt
+
+# ready DB
+./docker-readyDB.sh
+
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# ready DB
-./docker-readyDB.sh
-
-# start All2Rss
-python start.py >./running.log 2>&1  &
