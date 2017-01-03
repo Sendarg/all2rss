abfa7d841f65182674957159fc2d0a40371086b0
Sendarg Lee <Sendarg@users.noreply.github.com>
2017-01-03 22:34:48 +0800

    Current: Fuck  Docker

--- docker-readyDB.sh
+++ docker-readyDB.sh
@@ -7,5 +7,5 @@ docker run -d --name rss-redis -p 6379:6379 redis redis-server --requirepass vr2
 # ready for Neo4j
 mkdir -p $HOME/neo4j/data
 docker run -d --name rss-neo4j -p 7474:7474 -p 7687:7687 --volume=$HOME/neo4j/data:/data neo4j
-sleep 1
+sleep 2
 curl -H "Content-Type: application/json" -X POST -d '{"password":"neo4321"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-
-# clean up
-docker rmi -f $(docker images -qf "dangling=true")
-docker stop all2rss && docker rmi -f all2rss && docker rm -f all2rss
-
 # ready DB
 ./docker-readyDB.sh
 
 # start All2Rss
-python start.py
+python start.py >./running.log 2>&1  &
